c177238f4236f23a5a30cf805275b205b146604b
Sir Sergey <Sergeyandreev22@gmail.com>
2022-10-18 01:50:47 +0300

    homework_3

--- 2_ex.py
+++ 2_ex.py
@@ -1,30 +1,17 @@
-""" Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-Пример:
-- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) """
+# Напишите программу, которая найдёт произведение пар чисел списка. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
 
+list = [2, 3, 6, 4, 3, 5]
 
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = int(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Число должно быть integer ")
-    return number
-
-
-def mult(n):
-    if n == 1:
-        return 1
-    else:
-        return n * mult(n - 1)
-
-
-num = InputNumbers("Введите число: ")
-
-list = []
-for e in range(1, num + 1):
-    list.append(mult(e))
-
-print(f"Произведения чисел от 1 до {num}:  {list}")
+# if len(list)%2 == 0:
+#     size = len(list) // 2
+import math 
+size = math.ceil(len(list)/2)
+print(size)
+list2 = []
+for i in range(size):
+    list2.append(list[i]*list[-i - 1])
+print(list2)
